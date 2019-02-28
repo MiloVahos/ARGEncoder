d5204d58aa560d40a3c214b9934e1760cbfe88eb
Raykgeneer96 <juancamilopv@hotmail.com>
2019-02-27 23:20:35 -0500

    Pruebas con maximo de errores

--- runme.bash
+++ runme.bash
@@ -2,8 +2,8 @@
 
 for B in 100 20000000 100000000 200000000; do
     
-    mkdir "Prueba con $B y 03625"
-    time ./ARF -DATA GRCh38.fa -I -Q -L 1024 -B $B -C 1 -P0 0.3625
+    mkdir "Prueba con $B y 002"
+    time ./ARF -DATA GRCh38.fa -I -Q -L 1024 -B $B -C 1 -P0 0.02
     echo "Completada prueba con $B"
     mv "GRCh38.meta" "Prueba con $B y 03625"
     mv "GRCh38.align" "Prueba con $B y 03625"
