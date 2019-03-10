c1f180d779d7beb0b5059193fd06f460f7d42d18
Raykgeneer96 <juancamilopv@hotmail.com>
2019-03-10 18:50:54 -0500

    Cambio a 1024

--- runme.bash
+++ runme.bash
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 for B in 1000000 10000000 50000000 100000000; do
-    mkdir "L256_K26_B$B"
-    time ./ARF -DATA GRCh38.fa -I -Q -L 256 -B $B -C 1 -P0 0.1075
-    echo "Completada L256_K26_B$B"
-    mv "GRCh38.meta" "L256_K26_B$B"
-    mv "GRCh38.align" "L256_K26_B$B"
+    mkdir "L1024_K26_B$B"
+    time ./ARF -DATA GRCh38.fa -I -Q -L 1024 -B $B -C 1 -P0 0.1075
+    echo "Completada L1024_K26_B$B"
+    mv "GRCh38.meta" "L1024_K26_B$B"
+    mv "GRCh38.align" "L1024_K26_B$B"
 done
 
 for B in 1000000 10000000 50000000 100000000; do
-    mkdir "L256_K64_B$B"
-    time ./ARF -DATA GRCh38.fa -I -Q -L 256 -B $B -C 1 -P0 0.0575
-    echo "Completada L256_K64_B$B"
-    mv "GRCh38.meta" "L256_K64_B$B"
-    mv "GRCh38.align" "L256_K64_B$B"
+    mkdir "L1024_K64_B$B"
+    time ./ARF -DATA GRCh38.fa -I -Q -L 1024 -B $B -C 1 -P0 0.0575
+    echo "Completada L1024_K64_B$B"
+    mv "GRCh38.meta" "L1024_K64_B$B"
+    mv "GRCh38.align" "L1024_K64_B$B"
 done
 
 for B in 1000000 10000000 50000000 100000000; do
-    mkdir "L256_K128_B$B"
-    time ./ARF -DATA GRCh38.fa -I -Q -L 256 -B $B -C 1 -P0 0.0325
-    echo "Completada L256_K128_B$B"
-    mv "GRCh38.meta" "L256_K128_B$B"
-    mv "GRCh38.align" "L256_K128_B$B"
+    mkdir "L1024_K128_B$B"
+    time ./ARF -DATA GRCh38.fa -I -Q -L 1024 -B $B -C 1 -P0 0.0325
+    echo "Completada L1024_K128_B$B"
+    mv "GRCh38.meta" "L1024_K128_B$B"
+    mv "GRCh38.align" "L1024_K128_B$B"
 done
 
 
