9bed485949ed27f27b2746f4433fa3de47377dff
Raykgeneer96 <juancamilopv@hotmail.com>
2019-05-05 12:27:53 -0500

    Pruebas

--- runme.bash
+++ runme.bash
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 for B in 10000000 50000000 100000000; do
-    mkdir "L512_K26_B$B"
-    time ./ARF -DATA GRCh38.fa -I -Q -L 512 -B $B -C 1 -P0 0.1075
-    echo "Completada L512_K26_B$B"
-    mv "GRCh38.meta" "L512_K26_B$B"
-    mv "GRCh38.align" "L512_K26_B$B"
+    mkdir "FPL1024_K26_B$B"
+    time ./ARF -DATA GRCh38.fa -I -Q -L 1024 -B $B -C 1 -P0 0.1075
+    echo "Completada L1024_K26_B$B"
+    mv "GRCh38.meta" "L1024_K26_B$B"
+    mv "GRCh38.align" "L1024_K26_B$B"
 done
 
 for B in 10000000 50000000 100000000; do
-    mkdir "L512_K128_B$B"
-    time ./ARF -DATA GRCh38.fa -I -Q -L 512 -B $B -C 1 -P0 0.0325
-    echo "Completada L512_K128_B$B"
-    mv "GRCh38.meta" "L512_K128_B$B"
-    mv "GRCh38.align" "L512_K128_B$B"
+    mkdir "FPL1024_K128_B$B"
+    time ./ARF -DATA GRCh38.fa -I -Q -L 1024 -B $B -C 1 -P0 0.0325
+    echo "Completada L1024_K128_B$B"
+    mv "GRCh38.meta" "L1024_K128_B$B"
+    mv "GRCh38.align" "L1024_K128_B$B"
+done
+
+for B in 10000000 50000000 100000000; do
+    mkdir "FPL1024_K256_B$B"
+    time ./ARF -DATA GRCh38.fa -I -Q -L 1024 -B $B -C 1 -P0 0.02
+    echo "Completada L1024_K256_B$B"
+    mv "GRCh38.meta" "L1024_K256_B$B"
+    mv "GRCh38.align" "L1024_K256_B$B"
 done
 
 
